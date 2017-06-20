a1ed27731954338dd2aa7c48a718d1b97972731b
innfinite4evr <innfinite4evr@gmail.com>
2017-06-20 16:18:03 +0530

    Include device/sample and vendor/nxp

--- sync_d6503.sh
+++ sync_d6503.sh
@@ -6,6 +6,8 @@ rm -rf device/sony/common-init
 rm -rf device/sony/common-headers
 rm -rf device/sony/sepolicy
 rm -rf kernel/sony/msm/*
+rm -rf vendor/nxp
+rm -rf device/sample
 git clone https://github.com/sonyxperiadev/device-sony-sirius device/sony/sirius
 git clone https://github.com/sonyxperiadev/device-sony-shinano device/sony/shinano
 git clone https://github.com/sonyxperiadev/device-sony-common device/sony/common
@@ -13,3 +15,5 @@ git clone https://github.com/sonyxperiadev/device-sony-common-init device/sony/c
 git clone https://github.com/sonyxperiadev/device-sony-common-headers device/sony/common-headers
 git clone https://github.com/sonyxperiadev/device-sony-sepolicy device/sony/sepolicy
 git clone https://github.com/sonyxperiadev/kernel kernel/sony/msm
+git clone https://github.com/sonyxperiadev/vendor-nxp vendor/nxp
+git clone https://android.googlesource.com/device/sample device/sample
