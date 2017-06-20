03d74c4b22cd322481353b5de8eb0a138df0f4a8
innfinite4evr <innfinite4evr@gmail.com>
2017-06-20 14:50:15 +0530

    Update sync_d6503.sh

--- sync_d6503.sh
+++ sync_d6503.sh
@@ -2,7 +2,6 @@
 rm -rf device/sony/sirius
 rm -rf device/sony/shinano
 rm -rf device/sony/common
-rm -rf device/sony/common-kernel
 rm -rf device/sony/common-init
 rm -rf device/sony/common-headers
 rm -rf device/sony/sepolicy
@@ -12,6 +11,5 @@ git clone https://github.com/sonyxperiadev/device-sony-shinano device/sony/shina
 git clone https://github.com/sonyxperiadev/device-sony-common device/sony/common
 git clone https://github.com/sonyxperiadev/device-sony-common-init device/sony/common-init
 git clone https://github.com/sonyxperiadev/device-sony-common-headers device/sony/common-headers
-git clone https://github.com/sonyxperiadev/device-sony-common-kernel device/sony/common-kernel
 git clone https://github.com/sonyxperiadev/device-sony-sepolicy device/sony/sepolicy
 git clone https://github.com/sonyxperiadev/kernel kernel/sony/msm
